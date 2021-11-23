80f327b78a1d8f1876fb2f368fab48b6b09f45a2
Alex Charland <ac3charland@gmail.com>
2021-11-23 13:30:05 -0600

    Fix CoffeesService unit test. Fix related imports

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto'
 import { Connection, Repository } from 'typeorm'
@@ -6,10 +6,7 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto'
 import { UpdateCoffeeDto } from './dto/update-coffee.dto'
 import { Coffee } from './entities/coffee.entity'
 import { Flavor } from './entities/flavor.entity'
-import { Event } from 'src/events/entities/event.entity'
-import { COFFEE_BRANDS } from './coffees.constants'
-import { ConfigType } from '@nestjs/config'
-import coffeesConfig from './config/coffees.config'
+import { Event } from '../events/entities/event.entity'
 
 @Injectable()
 export class CoffeesService {
@@ -19,9 +16,6 @@ export class CoffeesService {
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
         private readonly connection: Connection,
-        @Inject(COFFEE_BRANDS) coffeeBrands: string[],
-        @Inject(coffeesConfig.KEY)
-        private readonly coffeesConfiguration: ConfigType<typeof coffeesConfig>,
     ) {}
 
     findAll(paginationQuery: PaginationQueryDto) {
